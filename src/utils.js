293b8ff3d4a3ae1adbe90c0ae257ff116e22e773
Jakob Miland <saebekassebil@gmail.com>
2013-05-09 19:24:46 +0200

    Implement triad inferer

---
+++ src/utils.js
@@ -0,0 +1,41 @@
+/**
+ * getDistance, returns the distance in semitones between two notes
+ **/
+function getDistance(from, to) {
+  from = kNotes[from];
+  to = kNotes[to];
+  if (from.distance > to.distance) {
+    return (to.distance + 12) - from.distance;
+  } else {
+    return to.distance - from.distance;
+  }
+}
+
+/**
+ * pad, returns the string, padded with character ch
+ **/
+function pad(str, ch, len) {
+  for (; len > 0; len--) {
+    str += ch;
+  }
+
+  return str;
+}
+
+/**
+ * compareArray, shallow matches two arrays
+ **/
+function compareArray(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  for (var i = 0, length = first.length; i < length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
