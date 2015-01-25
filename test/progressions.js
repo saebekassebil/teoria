25ee22a44462daa45eaea536326e78b8da7571a1
Jason Storey <jason.andrew.storey@gmail.com>
2015-01-25 20:40:09 +0000

    Reformat indentation using project style, and remove unused help function

--- test/progressions.js
+++ test/progressions.js
@@ -2,50 +2,45 @@ var vows = require('vows'),
     assert = require('assert'),
     teoria = require('../dist/teoria.js');
 
-// Utility function
-function simpleArray(chord) {
-    return chord.notes().map(function(n) { return n.toString(true); });
-}
-
 vows.describe('Progressions').addBatch({
-    'Progression parser': {
-        'C Major': function() {
-            var scale = teoria.scale('c3', 'ionian'),
-                twoFiveOne = teoria.progression(scale, [2,5,1]);
+  'Progression parser': {
+    'C Major': function() {
+      var scale = teoria.scale('c3', 'ionian'),
+          twoFiveOne = teoria.progression(scale, [2, 5, 1]);
 
-            assert.deepEqual(twoFiveOne.simple(), [['d3','f3','a3'],['g3','b3','d3'],['c3','e3','g3']]);
-        },
-        'A Minor': function() {
-            var scale = teoria.scale('a3', 'aeolian'),
-                twoFiveOne = teoria.progression(scale, [2,5,1]);
-
-            assert.deepEqual(twoFiveOne.simple(), [['b3','d4','f4'],['e4','g4','b3'],['a3','c4','e4']]);
-        },
-        'Db Major': function() {
-            var scale = teoria.scale('db3', 'ionian'),
-                twoFiveOne = teoria.progression(scale, [2,5,1]);
-
-            assert.deepEqual(twoFiveOne.simple(), [['eb3','gb3','bb3'],['ab3','c4','eb3'],['db3','f3','ab3']]);
-        }
+      assert.deepEqual(twoFiveOne.simple(), [['d3', 'f3', 'a3'], ['g3', 'b3', 'd3'], ['c3', 'e3', 'g3']]);
     },
-    'Creates chords with correct root': {
-        'First chord': function() {
-            var scale = teoria.scale('c4', 'major'),
-                twoFiveOne = teoria.progression(scale, [2,5,1]);
+    'A Minor': function() {
+      var scale = teoria.scale('a3', 'aeolian'),
+          twoFiveOne = teoria.progression(scale, [2, 5, 1]);
 
-            assert.equal(twoFiveOne.getChord(0).root.toString(), 'd4');
-        },
-        'Second chord': function() {
-            var scale = teoria.scale('c4', 'major'),
-                twoFiveOne = teoria.progression(scale, [2,5,1]);
+      assert.deepEqual(twoFiveOne.simple(), [['b3', 'd4', 'f4'], ['e4', 'g4', 'b3'], ['a3', 'c4', 'e4']]);
+    },
+    'Db Major': function() {
+      var scale = teoria.scale('db3', 'ionian'),
+          twoFiveOne = teoria.progression(scale, [2, 5, 1]);
 
-            assert.equal(twoFiveOne.getChord(1).root.toString(), 'g4');
-        },
-        'Third chord': function() {
-            var scale = teoria.scale('c4', 'major'),
-                twoFiveOne = teoria.progression(scale, [2,5,1]);
-
-            assert.equal(twoFiveOne.getChord(2).root.toString(), 'c4');
-        }
+      assert.deepEqual(twoFiveOne.simple(), [['eb3', 'gb3', 'bb3'], ['ab3', 'c4', 'eb3'], ['db3', 'f3', 'ab3']]);
     }
+  },
+  'Creates chords with correct root': {
+    'First chord': function() {
+      var scale = teoria.scale('c4', 'major'),
+          twoFiveOne = teoria.progression(scale, [2, 5, 1]);
+
+      assert.equal(twoFiveOne.getChord(0).root.toString(), 'd4');
+    },
+    'Second chord': function() {
+      var scale = teoria.scale('c4', 'major'),
+          twoFiveOne = teoria.progression(scale, [2, 5, 1]);
+
+      assert.equal(twoFiveOne.getChord(1).root.toString(), 'g4');
+    },
+    'Third chord': function() {
+      var scale = teoria.scale('c4', 'major'),
+          twoFiveOne = teoria.progression(scale, [2, 5, 1]);
+
+      assert.equal(twoFiveOne.getChord(2).root.toString(), 'c4');
+    }
+  }
 }).export(module);
