51c94e2dbeb20a21cd56592ca40477796a155f63
Matthew Hasbach <hasbach.git@gmail.com>
2015-08-02 07:59:55 -0400

    Chord.simple is now leveraged in tests

--- test/chords.js
+++ test/chords.js
@@ -2,312 +2,241 @@ var vows = require('vows'),
     assert = require('assert'),
     teoria = require('../');
 
-// Utility function
-function simpleArray(chord) {
-  return chord.notes().map(function(n) { return n.toString(true); });
-}
-
 vows.describe('Chords').addBatch({
   'Chord parser': {
     'Emaj7': function() {
-      var notes = simpleArray(teoria.chord('Emaj7'));
-      assert.deepEqual(notes, ['e', 'g#', 'b', 'd#']);
+      assert.deepEqual(teoria.chord('Emaj7').simple(), ['e', 'g#', 'b', 'd#']);
     },
 
     'A+': function() {
-      var notes = simpleArray(teoria.chord('A+'));
-      assert.deepEqual(notes, ['a', 'c#', 'e#']);
+      assert.deepEqual(teoria.chord('A+').simple(), ['a', 'c#', 'e#']);
     },
 
     'Bb+': function() {
-      var notes = simpleArray(teoria.chord('Bb+'));
-      assert.deepEqual(notes, ['bb', 'd', 'f#']);
+      assert.deepEqual(teoria.chord('Bb+').simple(), ['bb', 'd', 'f#']);
     },
 
     'F#maj7': function() {
-      var notes = simpleArray(teoria.chord('F#maj7'));
-      assert.deepEqual(notes, ['f#', 'a#', 'c#', 'e#']);
+      assert.deepEqual(teoria.chord('F#maj7').simple(), ['f#', 'a#', 'c#', 'e#']);
     },
 
     'Hmaj7': function() {
-      var notes = simpleArray(teoria.chord('Hmaj7'));
-      assert.deepEqual(notes, ['b', 'd#', 'f#', 'a#']);
+      assert.deepEqual(teoria.chord('Hmaj7').simple(), ['b', 'd#', 'f#', 'a#']);
     },
 
     'H#maj7': function() {
-      var notes = simpleArray(teoria.chord('H#maj7'));
-      assert.deepEqual(notes, ['b#', 'dx', 'fx', 'ax']);
+      assert.deepEqual(teoria.chord('H#maj7').simple(), ['b#', 'dx', 'fx', 'ax']);
     },
 
     'C7b5': function() {
-      var notes = simpleArray(teoria.chord('C7b5'));
-      assert.deepEqual(notes, ['c', 'e', 'gb', 'bb']);
+      assert.deepEqual(teoria.chord('C7b5').simple(), ['c', 'e', 'gb', 'bb']);
     },
 
     'Eb7b5': function() {
-      var notes = simpleArray(teoria.chord('Eb7b5'));
-      assert.deepEqual(notes, ['eb', 'g', 'bbb', 'db']);
+      assert.deepEqual(teoria.chord('Eb7b5').simple(), ['eb', 'g', 'bbb', 'db']);
     },
 
     'D#7b5': function() {
-      var notes = simpleArray(teoria.chord('D#7b5'));
-      assert.deepEqual(notes, ['d#', 'fx', 'a', 'c#']);
+      assert.deepEqual(teoria.chord('D#7b5').simple(), ['d#', 'fx', 'a', 'c#']);
     },
 
     'C9': function() {
-      var notes = simpleArray(teoria.chord('C9'));
-      assert.deepEqual(notes, ['c', 'e', 'g', 'bb', 'd']);
+      assert.deepEqual(teoria.chord('C9').simple(), ['c', 'e', 'g', 'bb', 'd']);
     },
 
     'Eb9': function() {
-      var notes = simpleArray(teoria.chord('Eb9'));
-      assert.deepEqual(notes, ['eb', 'g', 'bb', 'db', 'f']);
+      assert.deepEqual(teoria.chord('Eb9').simple(), ['eb', 'g', 'bb', 'db', 'f']);
     },
 
     'G#(#9)': function() {
-      var notes = simpleArray(teoria.chord('G#(#9)'));
-      assert.deepEqual(notes, ['g#', 'b#', 'd#', 'f#', 'ax']);
+      assert.deepEqual(teoria.chord('G#(#9)').simple(), ['g#', 'b#', 'd#', 'f#', 'ax']);
     },
 
     'Ab(b9)': function() {
-      var notes = simpleArray(teoria.chord('Ab(b9)'));
-      assert.deepEqual(notes, ['ab', 'c', 'eb', 'gb', 'bbb']);
+      assert.deepEqual(teoria.chord('Ab(b9)').simple(), ['ab', 'c', 'eb', 'gb', 'bbb']);
     },
 
     'F#(#11)': function() {
-      var notes = simpleArray(teoria.chord('F#(#11)'));
-      assert.deepEqual(notes, ['f#', 'a#', 'c#', 'e', 'g#', 'b#']);
+      assert.deepEqual(teoria.chord('F#(#11)').simple(), ['f#', 'a#', 'c#', 'e', 'g#', 'b#']);
     },
 
     'Ab13': function() {
-      var notes = simpleArray(teoria.chord('Ab13'));
-      assert.deepEqual(notes, ['ab', 'c', 'eb', 'gb', 'bb', 'db', 'f']);
+      assert.deepEqual(teoria.chord('Ab13').simple(), ['ab', 'c', 'eb', 'gb', 'bb', 'db', 'f']);
     },
 
     'C7sus4': function() {
-      var notes = simpleArray(teoria.chord('C7sus4'));
-      assert.deepEqual(notes, ['c', 'f', 'g', 'bb']);
+      assert.deepEqual(teoria.chord('C7sus4').simple(), ['c', 'f', 'g', 'bb']);
     },
 
     'Cmaj9': function() {
-      var notes = simpleArray(teoria.chord('Cmaj9'));
-      assert.deepEqual(notes, ['c', 'e', 'g', 'b', 'd']);
+      assert.deepEqual(teoria.chord('Cmaj9').simple(), ['c', 'e', 'g', 'b', 'd']);
     },
 
     'Dmb6': function() {
-      var notes = simpleArray(teoria.chord('Dmb6'));
-      assert.deepEqual(notes, ['d', 'f', 'a', 'bb']);
+      assert.deepEqual(teoria.chord('Dmb6').simple(), ['d', 'f', 'a', 'bb']);
     },
 
     'C#(#5#9)': function() {
-      var notes = simpleArray(teoria.chord('C#(#5#9)'));
-      assert.deepEqual(notes, ['c#', 'e#', 'gx', 'b', 'dx']);
+      assert.deepEqual(teoria.chord('C#(#5#9)').simple(), ['c#', 'e#', 'gx', 'b', 'dx']);
     },
 
     'Cm(maj7)': function() {
-      var notes = simpleArray(teoria.chord('Cm(maj7)'));
-      assert.deepEqual(notes, ['c', 'eb', 'g', 'b']);
+      assert.deepEqual(teoria.chord('Cm(maj7)').simple(), ['c', 'eb', 'g', 'b']);
     },
 
     'F#m(11b5b9)': function() {
-      var notes = simpleArray(teoria.chord('F#m(11b5b9)'));
-      assert.deepEqual(notes, ['f#', 'a', 'c', 'e', 'g', 'b']);
+      assert.deepEqual(teoria.chord('F#m(11b5b9)').simple(), ['f#', 'a', 'c', 'e', 'g', 'b']);
     },
 
     'C/e': function() {
-      var notes = simpleArray(teoria.chord('C/e'));
-      assert.deepEqual(notes, ['e', 'c', 'g']);
+      assert.deepEqual(teoria.chord('C/e').simple(), ['e', 'c', 'g']);
     },
 
     'A7/g': function() {
-      var notes = simpleArray(teoria.chord('A7/g'));
-      assert.deepEqual(notes, ['g', 'a', 'c#', 'e']);
+      assert.deepEqual(teoria.chord('A7/g').simple(), ['g', 'a', 'c#', 'e']);
     },
 
     'G/f#': function() {
-      var notes = simpleArray(teoria.chord('G/f#'));
-      assert.deepEqual(notes, ['f#', 'g', 'b', 'd']);
+      assert.deepEqual(teoria.chord('G/f#').simple(), ['f#', 'g', 'b', 'd']);
     },
 
     'C6': function() {
-      var notes = simpleArray(teoria.chord('C6'));
-      assert.deepEqual(notes, ['c', 'e', 'g', 'a']);
+      assert.deepEqual(teoria.chord('C6').simple(), ['c', 'e', 'g', 'a']);
     },
 
     'A#6': function() {
-      var notes = simpleArray(teoria.chord('A#6'));
-      assert.deepEqual(notes, ['a#', 'cx', 'e#', 'fx']);
+      assert.deepEqual(teoria.chord('A#6').simple(), ['a#', 'cx', 'e#', 'fx']);
     },
 
     'Bb6': function() {
-      var notes = simpleArray(teoria.chord('Bb6'));
-      assert.deepEqual(notes, ['bb', 'd', 'f', 'g']);
+      assert.deepEqual(teoria.chord('Bb6').simple(), ['bb', 'd', 'f', 'g']);
     },
 
     'Am6': function() {
-      var notes = simpleArray(teoria.chord('Am6'));
-      assert.deepEqual(notes, ['a', 'c', 'e', 'f#']);
+      assert.deepEqual(teoria.chord('Am6').simple(), ['a', 'c', 'e', 'f#']);
     },
 
     'D(#6)': function() {
-      var notes = simpleArray(teoria.chord('D(#6)'));
-      assert.deepEqual(notes, ['d', 'f#', 'a', 'b#']);
+      assert.deepEqual(teoria.chord('D(#6)').simple(), ['d', 'f#', 'a', 'b#']);
     },
 
     'Eo': function() {
-      var notes = simpleArray(teoria.chord('Eo'));
-      assert.deepEqual(notes, ['e', 'g', 'bb']);
+      assert.deepEqual(teoria.chord('Eo').simple(), ['e', 'g', 'bb']);
     },
 
     'Eø': function() {
-      var notes = simpleArray(teoria.chord('Eø'));
-      assert.deepEqual(notes, ['e', 'g', 'bb', 'd']);
+      assert.deepEqual(teoria.chord('Eø').simple(), ['e', 'g', 'bb', 'd']);
     },
 
     'Do': function() {
-      var notes = simpleArray(teoria.chord('Do'));
-      assert.deepEqual(notes, ['d', 'f', 'ab']);
+      assert.deepEqual(teoria.chord('Do').simple(), ['d', 'f', 'ab']);
     },
 
     'Dø': function() {
-      var notes = simpleArray(teoria.chord('Dø'));
-      assert.deepEqual(notes, ['d', 'f', 'ab', 'c']);
+      assert.deepEqual(teoria.chord('Dø').simple(), ['d', 'f', 'ab', 'c']);
     },
 
     'Fo7': function() {
-      var notes = simpleArray(teoria.chord('Fo7'));
-      assert.deepEqual(notes, ['f', 'ab', 'cb', 'ebb']);
+      assert.deepEqual(teoria.chord('Fo7').simple(), ['f', 'ab', 'cb', 'ebb']);
     },
 
     'G#ø7': function() {
-      var notes = simpleArray(teoria.chord('G#ø7'));
-      assert.deepEqual(notes, ['g#', 'b', 'd', 'f#']);
+      assert.deepEqual(teoria.chord('G#ø7').simple(), ['g#', 'b', 'd', 'f#']);
     },
 
     'Cmin': function() {
-      var notes = simpleArray(teoria.chord('Cmin'));
-      assert.deepEqual(notes, ['c', 'eb', 'g']);
+      assert.deepEqual(teoria.chord('Cmin').simple(), ['c', 'eb', 'g']);
     },
 
     'Bmin11': function() {
-      var notes = simpleArray(teoria.chord('Bmin11'));
-      assert.deepEqual(notes, ['b', 'd', 'f#', 'a', 'c#', 'e']);
+      assert.deepEqual(teoria.chord('Bmin11').simple(), ['b', 'd', 'f#', 'a', 'c#', 'e']);
     },
 
     'C+M7': function() {
-      var notes = simpleArray(teoria.chord('C+M7'));
-      assert.deepEqual(notes, ['c', 'e', 'g#', 'b']);
+      assert.deepEqual(teoria.chord('C+M7').simple(), ['c', 'e', 'g#', 'b']);
     },
 
     'Bbdom7b5': function() {
-      var notes = simpleArray(teoria.chord('Bbdom7b5'));
-      assert.deepEqual(notes, ['bb', 'd', 'fb', 'ab']);
+      assert.deepEqual(teoria.chord('Bbdom7b5').simple(), ['bb', 'd', 'fb', 'ab']);
     },
 
     'E5': function() {
-      var notes = simpleArray(teoria.chord('E5'));
-      assert.deepEqual(notes, ['e', 'b']);
+      assert.deepEqual(teoria.chord('E5').simple(), ['e', 'b']);
     },
 
     'A5': function() {
-      var notes = simpleArray(teoria.chord('A5'));
-      assert.deepEqual(notes, ['a', 'e']);
+      assert.deepEqual(teoria.chord('A5').simple(), ['a', 'e']);
     },
 
     'C13#9b5': function() {
-      var notes = simpleArray(teoria.chord('C13#9b5'));
-      assert.deepEqual(notes, ['c', 'e', 'gb', 'bb', 'd#', 'f', 'a']);
+      assert.deepEqual(teoria.chord('C13#9b5').simple(), ['c', 'e', 'gb', 'bb', 'd#', 'f', 'a']);
     },
 
     'D13#5b9': function() {
-      var notes = simpleArray(teoria.chord('D13#5b9'));
-      assert.deepEqual(notes, ['d', 'f#', 'a#', 'c', 'eb', 'g', 'b']);
+      assert.deepEqual(teoria.chord('D13#5b9').simple(), ['d', 'f#', 'a#', 'c', 'eb', 'g', 'b']);
     },
 
     'C6/9': function() {
-      var notes = simpleArray(teoria.chord('C6/9'));
-      assert.deepEqual(notes, ['c', 'e', 'g', 'a', 'd']);
+      assert.deepEqual(teoria.chord('C6/9').simple(), ['c', 'e', 'g', 'a', 'd']);
     },
 
     'Ab6/9': function() {
-      var notes = simpleArray(teoria.chord('Ab6/9'));
-      assert.deepEqual(notes, ['ab', 'c', 'eb', 'f', 'bb']);
+      assert.deepEqual(teoria.chord('Ab6/9').simple(), ['ab', 'c', 'eb', 'f', 'bb']);
     },
 
     'CM7': function() {
-      var notes = simpleArray(teoria.chord('CM7'));
-      assert.deepEqual(notes, ['c', 'e', 'g', 'b']);
+      assert.deepEqual(teoria.chord('CM7').simple(), ['c', 'e', 'g', 'b']);
     },
 
     'CmM7': function() {
-      var notes = simpleArray(teoria.chord('CmM7'));
-      assert.deepEqual(notes, ['c', 'eb', 'g', 'b']);
+      assert.deepEqual(teoria.chord('CmM7').simple(), ['c', 'eb', 'g', 'b']);
     },
 
     'DM': function() {
-      var notes = simpleArray(teoria.chord('DM'));
-      assert.deepEqual(notes, ['d', 'f#', 'a']);
+      assert.deepEqual(teoria.chord('DM').simple(), ['d', 'f#', 'a']);
     },
 
     'EM#5': function() {
-      var notes = simpleArray(teoria.chord('EM#5'));
-      assert.deepEqual(notes, ['e', 'g#', 'b#']);
+      assert.deepEqual(teoria.chord('EM#5').simple(), ['e', 'g#', 'b#']);
     },
 
     'FM9': function() {
-      var notes = simpleArray(teoria.chord('FM9'));
-      assert.deepEqual(notes, ['f', 'a', 'c', 'e', 'g']);
+      assert.deepEqual(teoria.chord('FM9').simple(), ['f', 'a', 'c', 'e', 'g']);
     },
 
     'Dmi': function() {
-      var notes = simpleArray(teoria.chord('Dmi'));
-      assert.deepEqual(notes, ['d', 'f', 'a']);
+      assert.deepEqual(teoria.chord('Dmi').simple(), ['d', 'f', 'a']);
 
     },
 
     'Emi7': function() {
-      var notes = simpleArray(teoria.chord('Emi7'));
-      assert.deepEqual(notes, ['e', 'g', 'b', 'd']);
+      assert.deepEqual(teoria.chord('Emi7').simple(), ['e', 'g', 'b', 'd']);
     },
 
     'Dma': function() {
-      var notes = simpleArray(teoria.chord('Dma'));
-      assert.deepEqual(notes, ['d', 'f#', 'a']);
+      assert.deepEqual(teoria.chord('Dma').simple(), ['d', 'f#', 'a']);
     },
 
     'Ema9': function() {
-      var notes = simpleArray(teoria.chord('Ema9'));
-      assert.deepEqual(notes, ['e', 'g#', 'b', 'd#', 'f#']);
+      assert.deepEqual(teoria.chord('Ema9').simple(), ['e', 'g#', 'b', 'd#', 'f#']);
     }
   },
 
   'Case doesn\'t matter': {
     'BbDom': function() {
-      assert.deepEqual(
-        simpleArray(teoria.chord('BbDom')),
-        simpleArray(teoria.chord('Bbdom'))
-      );
+      assert.deepEqual(teoria.chord('BbDom').simple(), teoria.chord('Bbdom').simple());
     },
 
     'EbMaj9': function() {
-      assert.deepEqual(
-        simpleArray(teoria.chord('EbMaj9')),
-        simpleArray(teoria.chord('Ebmaj9'))
-      );
+      assert.deepEqual(teoria.chord('EbMaj9').simple(), teoria.chord('Ebmaj9').simple());
     },
 
     'ASus4': function() {
-      assert.deepEqual(
-        simpleArray(teoria.chord('ASus4')),
-        simpleArray(teoria.chord('Asus4'))
-      );
+      assert.deepEqual(teoria.chord('ASus4').simple(), teoria.chord('Asus4').simple());
     },
 
     'EAdd9': function() {
-      assert.deepEqual(
-        simpleArray(teoria.chord('EAdd9')),
-        simpleArray(teoria.chord('Eadd9'))
-      );
+      assert.deepEqual(teoria.chord('EAdd9').simple(), teoria.chord('Eadd9').simple());
     }
   },
 
